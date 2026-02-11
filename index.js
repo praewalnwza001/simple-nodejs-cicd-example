2b168137036d022c710b52a28131cca54c3424a2
praewalnwza001 <sokcamin@gmail.com>
2026-02-11 14:40:17 +0700

    Add

--- index.js
+++ index.js
@@ -1,36 +1,11 @@
 const express = require('express');
-const port = process.env.PORT || 8000;
 const app = express();
-
-var SplitFactory = require('@splitsoftware/splitio').SplitFactory;
-
-var factory = SplitFactory({
-    core: {
-        authorizationKey: process.env.SPLIT_API_KEY
-    }
-});
-var splitClient = factory.client();
-
-var getTreatment = function() {
-    return splitClient.getTreatment('ANONYMOUS_USER', 'hello-treatment');
-}
-
-splitClient.on(splitClient.Event.SDK_READY, function () {
-    console.log('split.io sdk is ready');
-    console.log('treatment is: ' + getTreatment());
-});
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    let treatment = getTreatment();
-    if (treatment == 'on') {
-        res.send('Hello, Your Treatment is ON!');
-    } else if (treatment == 'off') {
-        res.send('Hello, Your Treatment is OFF!');
-    } else {
-        res.send('Hello, This Is The Default!');
-    }
+  res.send('<h1>Sawasdee Zin! Exam Passed 100%</h1><p>Jenkins to Vercel Success!</p>');
 });
 
-app.listen(port, () => { console.log('Our App Is Up And Running!'); });
-
-module.exports = app
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
